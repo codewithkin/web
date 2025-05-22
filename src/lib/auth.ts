695d52d4bced96a9d0ce4e977633051ee0dba92e
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-22 14:28:34 +0200

    Added some stuff, hope it still works

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,12 @@
 import {betterAuth} from "better-auth";
 import {prismaAdapter} from "better-auth/adapters/prisma";
+import {nextCookies} from "better-auth/next-js";
+import {magicLink} from "better-auth/plugins";
+import {sendMagicLinkEmail} from "./email/sendMagicLink";
 import {PrismaClient} from "@/generated/prisma";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
@@ -13,4 +17,10 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [
+    magicLink({
+      sendMagicLink: sendMagicLinkEmail,
+    }),
+    nextCookies(), // Hey Kin, make sure this is always the last plugin, or else it won't work
+  ],
 });
