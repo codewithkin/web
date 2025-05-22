dc632f89e368b409fe78a57b6e82184c8f45c919
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-05-22 14:46:37 +0200

    [ADD]: Test signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,20 @@
 "use client";
+import { authClient } from "@/lib/auth-client"
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { authClient } from "@/lib/auth-client";
-import { Loader2 } from "lucide-react";
-
-function LandingPage() {
-  const router = useRouter();
-  const { data, isPending } = authClient.useSession();
-
-  useEffect(() => {
-    if (isPending) return;
-    if (data) {
-      router.push("/dashboard");
-    } else {
-      router.push("/auth");
-    }
-  }, [data, isPending, router]);
-
-  if (isPending) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <Loader2 className="animate-spin h-6 w-6 text-muted-foreground" />
-      </div>
-    );
-  }
-
-  return null;
+const signIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google"
+  })
 }
 
-export default LandingPage;
+function Home() {
+  return (
+    <div>
+      <h2>Hi</h2>
+
+      <button onClick={() => signIn()}>Sign in</button>
+    </div>
+  )
+}
+
+export default Home
